b9f142cb6246fb7f440cef5b57544961f269277f
EthanArmbrust <ethanarm@buffalo.edu>
2017-02-14 21:47:15 -0500

    fixed windows dll issues

--- buildForWindows.sh
+++ buildForWindows.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-i686-w64-mingw32-g++-posix -std=c++11 -c ./src/Random.cpp ./src/windowsMain.cpp  ./src/logChecker.cpp -O3 -lpthread
+i686-w64-mingw32-g++-posix -std=c++11 -c ./src/Random.cpp ./src/windowsMain.cpp  ./src/logChecker.cpp -O3
 cd src
 i686-w64-mingw32-windres sgIcon.rc -O coff -o icon.res
 cd ..
-i686-w64-mingw32-g++-posix -o SeedGenerator.exe Random.o logChecker.o windowsMain.o ./src/icon.res -static-libgcc -static-libstdc++ 
+i686-w64-mingw32-g++-posix -o SeedGenerator.exe Random.o logChecker.o windowsMain.o ./src/icon.res -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic
 rm Random.o logChecker.o windowsMain.o
