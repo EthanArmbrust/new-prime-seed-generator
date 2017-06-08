e76c0bf0c558e6df189e42d0b0ac45def1d5b050
EthanArmbrust <ethanarm@buffalo.edu>
2017-06-08 13:02:37 -0400

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-g++ -c --std=c++11 .\src\main.cpp .\src\logChecker.cpp .\src\Random.cpp
+g++ -c --std=c++11 .\src\main.cpp .\src\logChecker.cpp .\src\Random.cpp .\src\BigInteger\BigUnsigned.cc .\src\BigInteger\BigUnsignedInABase.cc .\src\BigInteger\BigIntegerUtils.cc .\src\BigInteger\BigInteger.cc
 cd src
 windres sgIcon.rc -O coff -o icon.res
 cd ..
-g++ -o SeedGenerator.exe Random.o logChecker.o main.o .\src\icon.res -static-libgcc -static-libstdc++
-del Random.o logChecker.o main.o
+g++ -o SeedGenerator.exe Random.o logChecker.o main.o BigUnsigned.o BigIntegerUtils.o BigUnsignedInABase.o BigInteger.o .\src\icon.res -static-libgcc -static-libstdc++
+del Random.o logChecker.o main.o BigUnsigned.o BigIntegerUtils.o BigUnsignedInABase.o BigInteger.o
