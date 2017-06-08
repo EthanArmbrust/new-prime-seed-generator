e0a0eb164e152bbd0844b625037c689011956048
WorkstationAdmin <wksadmin@ubps-lck2n-m06.pubsites.buffalo.edu>
2017-06-08 12:45:16 -0400

    updated BigInt library to comply with c++11

--- buildForMac.sh
+++ buildForMac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -std=c++11 -stdlib=libc++ -c ./src/Random.cpp ./src/main.cpp ./src/logChecker.cpp -O3
-g++ -o SeedGeneratorMac Random.o logChecker.o main.o  -static-libstdc++ -pthread
-rm Random.o logChecker.o main.o
+clang++ -std=c++11 -stdlib=libc++ -c ./src/Random.cpp ./src/main.cpp ./src/logChecker.cpp ./src/BigInteger/BigUnsigned.cc ./src/BigInteger/BigUnsignedInABase.cc ./src/BigInteger/BigIntegerUtils.cc ./src/BigInteger/BigInteger.cc -O3
+clang++ -o SeedGeneratorMac Random.o logChecker.o main.o BigUnsigned.o BigIntegerUtils.o BigUnsignedInABase.o BigInteger.o 
+rm Random.o logChecker.o main.o BigUnsigned.o BigIntegerUtils.o BigUnsignedInABase.o BigInteger.o
